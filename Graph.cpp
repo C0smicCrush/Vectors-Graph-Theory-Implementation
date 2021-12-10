7f39f5e1ac76cd80d8aa6163c18f1d31cbcb8ad5
Aatmodhee Goswami <atomind2005@gmail.com>
2021-12-10 14:34:39 -0600

    DFS may actually work now...

--- Graph.cpp
+++ Graph.cpp
@@ -2,6 +2,7 @@
 Graph::Graph(){
 	nodes = 0;
 	edges = 0;
+	outSTR = "Hello";
 }
 int Graph::returnEdges(){
 	return edges;
@@ -9,16 +10,32 @@ int Graph::returnEdges(){
 int Graph::returnNodes(){
 	return nodes;
 }
-bool Graph::addEdge(int primary, int secondary){
+bool Graph::addEdge(int primary, int secondary,int distance){
 	bool added = 0;
+	
 	if((std::binary_search(adjNums.begin(),adjNums.end(),primary)) && (std::binary_search(adjNums.begin(),adjNums.end(),secondary))){
-		bool added = 1;
+		int loc = returnInsertLocation(primary);
+		int count1  = adjList[loc]->getCount();
+		adjList[loc]->addNode(secondary, &outSTR, distance);
+		int count2 = adjList[loc]->getCount();
+		if(count1 < count2){
+			added = 1;
+			edges++;
+		}
 	}
+
+	return added; 
 }
 bool Graph::addVertex(int inVert){
 	bool added = 0;
 	if (!std::binary_search (adjNums.begin(), adjNums.end(), inVert)){
 		added = 1;
+		int insertLoc = returnInsertLocation(inVert);
+		LinkedList* newLL = new LinkedList;
+		auto pos = adjList.begin() + insertLoc;
+		adjList.insert(pos, newLL);
+		adjNums.insert(adjNums.begin() + insertLoc, inVert);
+		nodes++;
 	}
 	return added;
 }
@@ -32,4 +49,40 @@ int Graph::returnInsertLocation(int intIn){
 		}
 	}
 	return retInt;
+}
+void Graph::DFS(int startNode){
+	DFSprivate(startNode);
+	
+}
+void Graph::DFSprivate(int startNode){
+		vector<bool> visited(nodes, false);
+		Stack stack;
+		stack.push(startNode);
+		while(!stack.isEmpty()){
+			int top = stack.peek();
+			stack.pop();
+			int loc = returnInsertLocation(top);
+			if(!visited[loc]){
+				std::cout << top << " ";
+				visited[loc] = true;
+			}
+		int count = adjList[loc]->getCount();
+		int arr2[count];
+		int* arr = arr2;
+		adjList[loc]->returnArray(arr);
+		for(int x = 0; x < count; x++){
+			int loc = returnInsertLocation(arr2[x]);
+			if(!visited[loc]){
+				stack.push(arr2[x]);
+			}
+		}
+	}
+
+}
+Graph::~Graph(){
+	nodes = 0;
+	edges = 0;
+	for(int x = 0; nodes; x++){
+		delete adjList[x];
+	}
 }
