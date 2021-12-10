015d85d5160124d4f4d01277b5f78d3750a825e0
Aatmodhee Goswami <atomind2005@gmail.com>
2021-12-10 14:34:51 -0600

    Read Graph.cpp commit

--- Graph.h
+++ Graph.h
@@ -1,21 +1,26 @@
 #ifndef GRAPH_H
 #define GRAPH_H
-#include "linkedlist.cpp"
+#include "linkedlist.h"
+#include "stack.h"
 #include <vector>
 #include <algorithm>
+using std::vector;
 class Graph{
 	private:
 	int nodes,edges;
 	vector<LinkedList*> adjList;
 	vector<int> adjNums;
-	
+	string outSTR;
+	int returnInsertLocation(int);
+	void DFSprivate(int);
 	public:
+		void DFS(int);
 		Graph();
 		~Graph();
 		int returnNodes();
 		int returnEdges();
 		bool addVertex(int);
-		bool addEdge(int,int);
+		bool addEdge(int,int,int);
 		bool removeVertex(int);
 		bool removeEdge(int, int);
 			
