625ce54f8be0d9b4cc05459afd109ff008c1b349
Aatmodhee Goswami <atomind2005@gmail.com>
2021-12-10 16:13:03 -0600

    added testing

--- main.cpp
+++ main.cpp
@@ -1,21 +1,25 @@
 #include "Graph.h"
 int main(){
-	std::cout << "Hello ";
 	Graph graph;
 	graph.addVertex(1);
 	graph.addVertex(2);
 	graph.addVertex(3);
 	graph.addVertex(4);
 	graph.addVertex(5);
+	graph.addVertex(6);
 	graph.addEdge(1,2,3);
-	graph.addEdge(2,1,3);	
-	graph.addEdge(2,3,3);
-	graph.addEdge(3,1,3);
-	graph.addEdge(2,4,1);
+	graph.addEdge(2,3,3);	
+	graph.addEdge(3,4,3);
+	graph.addEdge(4,1,1);
 	graph.addEdge(2,5,1);
 	graph.addEdge(1,3,4);
-//	std::cout << graph.returnNodes();
-//	std::cout << graph.returnEdges() << " ";
+	graph.addEdge(5,6,4);
+	std::cout << std::endl << "Nodes: "<< graph.returnNodes() << std::endl;
+	std::cout << "Edges: " << graph.returnEdges() << " " << std::endl;
 	graph.DFS(2);
+	std::cout << std::endl;
+	graph.BFS(2);
+//	graph.DFS(1);
+//	graph.DFS(5);
 	return 0;
 }
