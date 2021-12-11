76508c6b1c3c41fe8ec30b464b4704e69cb10b27
JamesBremner <james@ravenspoint.com>
2021-12-11 12:26:13 -0500

    Add makefile

---
+++ makefile.mk
@@ -0,0 +1,43 @@
+
+# compiler include search paths
+INCS = ../
+
+
+
+SDIR = ..
+ODIR = ./obj
+
+OPT = -g
+CCflags = -std=c++17 $(OPT)
+STATLibs = -static-libstdc++ -static-libgcc -static
+
+### OBJECT FILES
+
+OBJ = $(ODIR)/main.o $(ODIR)/graph.o $(ODIR)/linkedlist.o $(ODIR)/stack.o 
+	
+
+### COMPILE
+
+$(ODIR)/main.o : main.cpp
+	g++ -o  $@ -c main.cpp $(INCS) $(CCflags)
+
+$(ODIR)/graph.o : graph.cpp
+	g++ -o  $@ -c graph.cpp $(INCS) $(CCflags)
+
+$(ODIR)/linkedlist.o : linkedlist.cpp
+	g++ -o  $@ -c linkedlist.cpp $(INCS) $(CCflags)
+
+$(ODIR)/stack.o : stack.cpp
+	g++ -o  $@ -c stack.cpp $(INCS) $(CCflags)
+
+#### LINKS
+
+test : $(OBJ)
+	g++ -o ./bin/test.exe \
+		$(OBJ) $(WEXLIBS) $(STATLibs) $(OPT)
+
+### CLEAN
+
+.PHONY: clean
+clean:
+	del /q ..\obj
